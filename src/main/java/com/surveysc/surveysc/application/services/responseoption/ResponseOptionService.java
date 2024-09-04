07c50c0935d8be3cb87afb20b69db4795735c2bd
silviaac28 <silviaangarita28@gmail.com>
2024-09-03 22:28:59 -0500

    actualiza respuestas

--- src/main/java/com/surveysc/surveysc/application/services/responseoption/ResponseOptionService.java
+++ src/main/java/com/surveysc/surveysc/application/services/responseoption/ResponseOptionService.java
@@ -1,9 +1,15 @@
 package com.surveysc.surveysc.application.services.responseoption;
 
+import java.util.Optional;
+
 import com.surveysc.surveysc.domain.entities.ResponseOption;
 
 public interface ResponseOptionService {
 
+    Optional<ResponseOption> findById(Long id);
+
     ResponseOption save(ResponseOption responseOption);
+
+    ResponseOption update(ResponseOption responseOption);
 
 }
---
+++ src/main/resources/Templates/js/responseoption.js/actualizarResponseOption.js
@@ -0,0 +1,38 @@
+function guardarEncuestaActualizar() {
+    const id = document.getElementById('idResponseOptionActualizar').value;
+    const optionValue = document.getElementById('optionValueActualizar').value;
+    const commentResponse = document.getElementById('commentResponseActualizar').value;
+    const optionText = document.getElementById('optionTextActualizar').value;
+    const questionId = document.getElementById('questionIdActualizar').value;
+
+
+    fetch('http://localhost:8080/responseoptionapi', { // Usando PUT para actualización
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            id : id,
+            option_value : optionValue,
+            commentResponse: commentResponse,
+            optionText: optionText,
+            questionId: questionId,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(text => {
+                throw new Error('Network response was not ok: ' + text);
+            });
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        alert('Opción de respuesta actualizada con éxito!');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error al actualizar la opción de respuesta.');
+    });
+}
